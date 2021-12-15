9fb597badc1ef9029ddc7f6c5616da39652f3623
Bas Broerse <bbroerse@Bass-MacBook-Pro-2.local>
2021-12-15 17:29:20 +0100

    Initial commit

---
+++ src/test/java/nl/teamrockstars/todoapi/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.teamrockstars.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
